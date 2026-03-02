877a12bed1f99524bd750378f3e6334383ed17f1
nKOxxx <98685501+nKOxxx@users.noreply.github.com>
2026-03-02 18:29:57 +0400

    feat: switch to Stamen Toner - grayscale land, black water tactical theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -36,9 +36,9 @@ function App() {
       zoomControl: false
     })
 
-    // Add dark tactical tile layer (CARTO Dark Matter - free)
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    // Add Stamen Toner tile layer (grayscale land, black water - tactical)
+    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
       subdomains: 'abcd',
       maxZoom: 19
     }).addTo(map)
